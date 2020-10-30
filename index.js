c73ed94e2bd69d8f2352692d0c9b472bb512fdaa
Bohyun Han <bohyunhan@Oss-MacBook-Pro.hitronhub.home>
2020-10-29 22:00:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb+srv://newUser:newUser@cluster0.ends1.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
